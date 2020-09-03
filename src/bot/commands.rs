eb87edefcb7e926482a15ab9d3b2d9c97b5cb5de
Matthew <staalkoper@gmail.com>
2020-09-03 11:53:27 +0200

    added embed example

--- src/bot/commands.rs
+++ src/bot/commands.rs
@@ -14,7 +14,7 @@ use crate::bot::utils::{reply};
 use crate::bot::DataBase;
 
 #[group()]
-#[commands(ping, db_test)]
+#[commands(ping, db_test, prefix)]
 pub struct Commands;
 
 #[command]
@@ -23,6 +23,28 @@ async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
     println!("{}", test);
 
     reply(&ctx, &msg, &String::from("Pong!")).await;
+    Ok(())
+}
+
+#[command]
+async fn prefix(ctx: &Context, msg: &Message) -> CommandResult {
+    let data = ctx.data.read().await;
+    let config = data.get::<Config>().unwrap();
+
+    if let Err(why) = msg.channel_id.send_message(&ctx.http,  |m| {
+        m.embed(|embed| {
+            embed.title("Prefix");
+            embed.description(format!("My prefix is: `{}`", &config.prefix));
+            embed.color(0xffa500)
+        });
+        m
+
+    }).await {
+        println!("Failed to send message in #{} because\n{:?}",
+                 msg.channel_id, why
+        );
+    };
+
     Ok(())
 }
 

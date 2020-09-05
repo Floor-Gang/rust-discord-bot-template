28a4df13643da1e373389445e64bb2529da35efd
Dylan Hackworth <dhpf@pm.me>
2020-09-05 12:13:38 -0500

    Added logging to Main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
+use crate::config::Config;
+use simple_logger::SimpleLogger;
+use log::{
+	LevelFilter, 
+	info, 
+	warn,
+};
+
+
 mod config;
 mod bot;
 mod database;
 
-use crate::config::Config;
 
 #[tokio::main]
 async fn main() {
+	SimpleLogger::new()
+        .with_level(LevelFilter::Info)
+		.init()
+		.unwrap();
+	
+	info!("Starting...");
+    
     let config = Config::new(String::from("./config.yml"));
+
+    if config.token.is_empty() {
+        warn!("Please fill out the config.yml");
+        return;
+    }
+
     bot::start(config).await;
 }
